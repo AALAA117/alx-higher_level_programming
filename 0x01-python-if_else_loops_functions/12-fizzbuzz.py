21af5e4548c030d417b254a5031ff78c3230acba
alaa <alaamglal@gmail.com>
2024-01-03 17:31:29 -0800

    c

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(1, 101):
+        if (i % 3 == 0):
+            print("{} ".format("Fizz"), end='')
+            continue
+        if (i % 5 == 0):
+            print("{} ".format("Buzz"), end='')
+            continue
+        if (i % 15 == 0):
+            print("{} ".format("FizzBuzz"), end='')
+            continue
+        else:
+            print("{:d} ".format(i), end='')
+            continue
