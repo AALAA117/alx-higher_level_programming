d236ad96fc6dc22da1f8c2984b80fb43e9d41dc0
alaa <alaamglal@gmail.com>
2024-01-30 12:17:16 -0800

    done

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module for size of square"""
+
+
+class Square():
+    """Define size of square"""
+    def __init__(self, size):
+        """get size of square"""
+        self.__size = size
