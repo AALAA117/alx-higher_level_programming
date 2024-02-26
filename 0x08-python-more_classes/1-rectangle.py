8f15926931097835beadb475906769b4013b4e32
aalaa <alaamglal@gmail.com>
2024-02-26 15:38:31 -0800

    done

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Module for a rectangle"""
+
+
+class Rectangle:
+    """define a rectangle"""
+    def __init__(self, width=0, height=0):
+        """initialize width and height"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """return width"""
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        """set width"""
+        if value != int(value):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """return height"""
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        """return height"""
+        if value != int(value):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__height = value
