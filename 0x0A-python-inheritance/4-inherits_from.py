9e659d6ba7cdac75783957fb840c0e2074bae850
aalaa <alaamglal@gmail.com>
2024-03-02 15:41:13 -0800

    done

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""inherited (directly or indirectly)"""
+
+
+def inherits_from(obj, a_class):
+    """ object is an instance of a class"""
+    if issubclass(type(obj), a_class):
+        if type(obj) is not a_class:
+            return (True)
+    else:
+        return (False)
