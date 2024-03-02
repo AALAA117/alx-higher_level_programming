a40d06eabe6826109a8143f6f90b5a3f7e5c22ff
aalaa <alaamglal@gmail.com>
2024-03-01 21:31:37 -0800

    done

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module for Name
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    define your name
+    """
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+    else:
+        print("My name is {} {}".format(first_name, last_name))
