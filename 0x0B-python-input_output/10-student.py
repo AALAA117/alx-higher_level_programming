e0c1e8e4cdef6a38a1d5483cecabc47603890d76
aalaa <alaamglal@gmail.com>
2024-03-05 14:08:30 -0800

    done

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""class Student"""
+
+
+class Student:
+    """defines a student"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student"""
+        if attrs:
+            dic = {}
+            for key in attrs:
+                if hasattr(self, key):
+                    dic[key] = getattr(self, key, None)
+            return (dic)
+        else:
+            return (self.__dict__)
