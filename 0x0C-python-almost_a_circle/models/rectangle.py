f26100599f19b6a83bf1602bfb2dad69d3d93462
aalaa <alaamglal@gmail.com>
2024-03-21 17:31:44 -0700

    done

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""Rectangle Model"""
+from .base import Base
+
+
+class Rectangle(Base):
+    """rectangle class"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """initialize"""
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        """get width"""
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        """set width"""
+        self.__width = value
+
+    @property
+    def height(self):
+        """get height"""
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        """set height"""
+        self.__height = value
+
+    @property
+    def x(self):
+        """get x"""
+        return (self.__x)
+
+    @x.setter
+    def x(self, value):
+        """set x"""
+        self.__x = value
+
+    @property
+    def y(self):
+        return (self.__y)
+
+    @y.setter
+    def y(self, value):
+        """set y"""
+        self.__y = value
