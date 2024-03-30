719fa59b96e479b5275092f7187c7517f8ddf56c
Aalaa <alaamglal@gmail.com>
2024-03-30 09:09:40 -0700

    done

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """BaseModel"""
+import json
 
 
 class Base:
@@ -13,3 +14,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """ returns the JSON string representation of list_dictionaries"""
+        if len(list_dictionaries) == 0 or list_dictionaries is None:
+            return ("[]")
+        else:
+            return (json.dumps(list_dictionaries))
