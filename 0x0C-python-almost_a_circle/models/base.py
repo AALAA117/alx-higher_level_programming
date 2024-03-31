9fc3b3c355a1546851387c4989dd12e244620fad
Aalaa <alaamglal@gmail.com>
2024-03-31 16:28:39 -0700

    done

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -18,7 +18,7 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """ returns the JSON string representation of list_dictionaries"""
-        if len(list_dictionaries) == 0 or type(list_dictionaries) is None:
+        if list_dictionaries is None or len(list_dictionaries) == 0:
             return ("[]")
         else:
             return (json.dumps(list_dictionaries))
