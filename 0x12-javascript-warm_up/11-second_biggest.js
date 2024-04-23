d97128ea77a11453620dc0e65d586095976b29c3
AALAA <alaamglal@gmail.com>
2024-04-23 02:52:04 -0700

    done

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,17 +1,20 @@
 #!/usr/bin/node
 const numOfArg = process.argv.length - 2;
-let largest = process.argv[2];
+let largest = parseInt(process.argv[2]);
 let secondLargest = -Infinity;
+let k;
+let num;
 if (numOfArg === 1 || numOfArg === 0) {
   console.log(0);
 } else {
   const lastIndex = process.argv.length - 1;
-  for (let k = 3; k <= lastIndex; k++) {
-    if (process.argv[k] > largest) {
+  for (k = 3; k <= lastIndex; k++) {
+    num = parseInt(process.argv[k]);
+    if (num > largest) {
       secondLargest = largest;
-      largest = process.argv[k];
-    } else if (process.argv[k] > secondLargest && process.argv[k] < largest) {
-      secondLargest = process.argv[k];
+      largest = num;
+    } else if (num > secondLargest && num < largest) {
+      secondLargest = num;
     }
   }
   console.log(secondLargest);
