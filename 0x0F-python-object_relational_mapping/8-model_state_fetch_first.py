945cdc5f51690008d0997050b3e35d57f99b1bf8
AALAA <alaamglal@gmail.com>
2024-04-26 11:41:39 -0700

    done

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Module contains the class definition of a State inherits from Base
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker, Session
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost/{}'
+            .format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    q = session.query(State).first()
+    print("{}: {}".format(q.id, q.name))
