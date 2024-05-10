06f4f27a9c3be5db3e1b3ec782aae20585a739f9
ALAA <alaamglal@gmail.com>
2024-05-10 09:23:35 -0700

    done

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -12,6 +12,6 @@ if __name__ == "__main__":
     url = argv[1]
     try:
         with urlopen(url) as response:
-            body = response.read().decode("utf-8")
+            print(response.read().decode("utf-8"))
     except HTTPError as error:
         print("Error code: {}".format(error.status))
