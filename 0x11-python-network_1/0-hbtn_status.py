041e3e312140429ae0142e112cf838562378ba8b
ALAA <alaamglal@gmail.com>
2024-05-10 07:36:04 -0700

    done

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module for sending requests using urllib"""
+from urllib.request import urlopen
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    with urlopen(url) as response:
+        body = response.read()
+
+    if isinstance(body, bytes):
+        content = body.decode("utf-8")
+    else:
+        content = body
+    print("Body response:")
+    print("    - type: {}".format(type(body)))
+    print("    - content: {}".format(body))
+    print("    - utf8 content: {}".format(content))
