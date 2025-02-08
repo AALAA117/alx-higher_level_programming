933a64e5d1d8ff3bc0b9be854ad0f41391a1273f
aalaa <alaamglal@gmail.com>
2025-02-08 19:02:50 +0200

    set 0 index to first input argument

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-from sys import argv
 if __name__ == "__main__":
-    if (len(argv) == 1):
-        print("{:d} arguments.".format(0))
+    from sys import argv
+    args = argv[1:]
+    if len(args) == 0:
+        print("0 arguments.")
+
     else:
-        if (len(argv) == 2):
-            print("{:d} argument:".format(1))
-            print("{:d}: {}".format(1, argv[1]))
+        if len(args) == 1:
+            print("1 argument:")
         else:
-            print("{:d} arguments:".format(len(argv) - 1))
-    if (len(argv) > 2):
-        for i in range(1, len(argv)):
-            print("{:d}: {}".format(i, argv[i]))
+            print("{:d} arguments:".format(len(args)))
+        for i in range(len(args)):
+            print("{:d}: {}".format(i+1, args[i]))
