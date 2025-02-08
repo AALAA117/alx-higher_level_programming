ac8d228cb4adcf644b928059da68b571b331adf4
aalaa <alaamglal@gmail.com>
2025-02-08 21:08:22 +0200

    add any number of arguments

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-from sys import argv
 if __name__ == "__main__":
+    from sys import argv
+    args = argv[1:]
     result = 0
-    if len(argv) == 1:
-        print("{:d}".format(0))
-    else:
-        for i in range(1, len(argv)):
-            result = result + int(argv[i])
-        print("{:d}".format(result))
+    if len(args) >= 1:
+        for i in range(len(args)):
+            result += int(args[i])
+    print(result)
